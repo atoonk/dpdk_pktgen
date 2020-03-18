4608b92d81a5320be46efddc77b43b8abd5ee42d
Andree Toonk <andree@toonk.nl>
2020-03-18 10:21:02 -0700

    Update monitorpkts.sh

--- monitorpkts.sh
+++ monitorpkts.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 
+if [ "$1" != "" ]; then
+	NIC=$1
+else
+    echo "Please provide the network interface you'd like to monitor."
+    echo "example: $0 eth0"
+    exit 1
+fi
 p=0
 d=0
 while sleep 1; do
-r=$(netstat -i | grep eno1 | awk '{print $3,$5}' | grep -v statistics)
+r=$(netstat -i | grep $NIC | awk '{print $3,$5}' | grep -v statistics)
 	p_now=$(echo $r | awk '{print $1}')
 	d_now=$(echo $r |awk '{print $2}')
 
