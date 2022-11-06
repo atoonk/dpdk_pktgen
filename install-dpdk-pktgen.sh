742dad34d9a4a35da38c173758669470886e7e8d
Andree Toonk <andree@toonk.nl>
2022-11-06 10:41:58 -0800

    Update install-dpdk-pktgen.sh

--- install-dpdk-pktgen.sh
+++ install-dpdk-pktgen.sh
@@ -69,6 +69,7 @@ apt-get -y install libnuma-dev
 apt-get -y install pciutils
 apt-get -y install libpcap-dev
 apt-get -y install liblua5.3-dev
+apt-get -y install python3-pyelftools
 apt -y install libelf-dev
 apt-get -y install linux-headers-`uname -r` || apt -y install  linux-headers-generic
 
